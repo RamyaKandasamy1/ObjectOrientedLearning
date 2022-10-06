41d608622fedefd6c4386a98714ea4a8731bb8b3
RamyaKandasamy1 <ramz87lakshmi@yahoo.co.in>
2022-10-05 22:16:13 -0500

    first commit

---
+++ src/Car.java
@@ -0,0 +1,17 @@
+
+public class Car {
+	
+	String name="Honda";
+	String model="CRV";
+	int year =2022;
+	String colour ="Black";
+	
+	void brake() {
+		System.out.println("You applied brake");
+		
+	}
+	void drive() {
+		System.out.println("You are driving the car");
+	}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car myCar=new Car();
+		System.out.println("Colour of my csr is " +myCar.colour);
+		
+		myCar.brake();
+
+	}
+
+}
